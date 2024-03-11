c24ffc0028b30cc50dcae043da62d9d31404e026
noahhan03 <noahhan0304@gmail.com>
2024-03-12 00:37:20 +0900

    week2 거의 끝

--- sql/create.sql
+++ sql/create.sql
@@ -4,22 +4,43 @@ USE eos_week2;
 
 -- --------------------------------
 
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS User;
 
-CREATE DATABASE IF NOT EXISTS User(
+CREATE TABLE IF NOT EXISTS User(
     userId VARCHAR(60),
     username VARCHAR(20),
     profileImg VARCHAR(200),
-    created  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ,
-    Updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (userId)
 );
 
 -- --------------------------------
 
-INSERT INTO User(userId, username) VALUES 
-("NoahHan", "한노아"),
-("RootPark","박근원"),
-("ming9","강민규"),
-("suah","수아"),
-("remi","다니엘");
+INSERT INTO User(userId, username, profileImg) VALUES 
+("NoahHan", "한노아", ''),
+("RootPark", "박근원", ''),
+("ming9", "강민규", ''),
+("asd", "수아", ''),
+("kim", "김기재", '');
+
+
+-- --------------------------------
+
+SELECT userId, username
+FROM User
+ORDER BY created
+DESC Limit 1;
+
+SELECT username
+FROM User
+WHERE userId = 'asd';
+
+DELETE FROM User
+WHERE username='박근원';
+
+UPDATE User
+SET userId = 'dsa'
+WHERE username = '김기재';
+
+
